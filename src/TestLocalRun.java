d117ea39b27b0b5b03d12fd9dec7a3b9aea257ea
swguru <swguru@DIN16002798.corp.capgemini.com>
2017-01-04 18:57:12 +0530

    commiting ant

---
+++ src/TestLocalRun.java
@@ -0,0 +1,42 @@
+
+
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.junit.After;
+import org.junit.Before;
+
+public class TestLocalRun {
+    private WebDriver driver;
+ 
+
+   
+   
+     @BeforeTest
+     public void setUp() {
+    	 System.setProperty("webdriver.chrome.driver","./lib/chromedriver.exe");
+    	 driver = new ChromeDriver();
+      
+       driver.get("http://www.google.com");
+     }
+     
+
+     @Test
+     public void testGoogleSearch() {	 
+    		   
+    		 driver.get("http://www.google.com"); 
+      
+     }
+     @AfterTest 
+     public void tearDown() {
+    	 driver.close();
+         driver.quit();
+     }
+   
+}
