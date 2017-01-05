4a203fa6da9c27e7b71923c3e6ea0faacee43e9b
swguru <swguru@DIN16002798.corp.capgemini.com>
2017-01-05 11:23:39 +0530

    commit

--- src/TestLocalRun.java
+++ src/TestLocalRun.java
@@ -32,7 +32,7 @@ public class TestLocalRun {
      public void testGoogleSearch() {	 
     		   
     		 driver.get("http://www.google.com"); 
-    		 Assert.assertEquals("Google45",driver.getTitle());
+    		 Assert.assertEquals("Google",driver.getTitle());
       
      }
      @AfterTest 
